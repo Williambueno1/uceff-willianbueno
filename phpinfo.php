6dab6869f50b0bf27581fff327396bcd6c29ecf1
Williambueno1 <williambueno1@hotmail.com>
2017-05-17 18:52:34 -0300

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,6 +1,10 @@
 <?php
-  
-  phpinfo
 
+// Mostra todas as informações, usa o padrão INFO_ALL
+phpinfo();
+
+// Mostra apenas informações dos módulos.
+// phpinfo(8) mostra um resultado identico.
+phpinfo(INFO_MODULES);
 
 ?>
